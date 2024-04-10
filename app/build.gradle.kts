6cc5ffde3e0913651ac1512d618fef3c0b836a43
Martin Hegel <martin.hegel@gmail.com>
2024-04-10 14:43:23 +0200

    dependencies implemented, retrofit implemented and base url implemented, model implemented

---
+++ app/build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+    id("com.android.application")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.example.thepagingapp2"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.thepagingapp2"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    // RXJava with retrofit
+    implementation("com.squareup.retrofit2:adapter-rxjava3:2.11.0")
+
+    // Paging
+    val paging_version = "3.1.1"
+    implementation("androidx.paging:paging-runtime:$paging_version")
+    // RxJava3 support
+    implementation("androidx.paging:paging-rxjava3:$paging_version")
+
+
+
+    // Hilt Dagger
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    annotationProcessor("com.google.dagger:hilt-compiler:2.51.1")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // Lifecycle
+    val lifecycle_version = "2.7.0"
+    val arch_version = "2.2.0"
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+}
---
+++ app/src/main/java/com/example/thepagingapp2/util/Utils.java
@@ -0,0 +1,6 @@
+package com.example.thepagingapp2.util;
+
+public class Utils {
+    public static String API_KEY = "ff2690cf1594d8697e59a85090760043";
+    public static String BASE_URL = "https://api.themoviedb.org/3/";
+}
